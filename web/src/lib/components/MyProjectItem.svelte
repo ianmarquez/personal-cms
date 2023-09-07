9ac736877b0968982be07bdf55f10b32547342bd
Ian Marquez <regilmarquez123@gmail.com>
2023-09-07 22:58:50 +0800

    Feat: Added my projects listing
    - added projects view
    - added projects delete feature
    - added MyProjectItem component for displaying project items

---
+++ web/src/lib/components/MyProjectItem.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import type { ProjectsResponse } from '$lib/types/pocketbase-types';
+	import { getImageUrl } from '$lib/utils';
+	import { Modal } from '.';
+
+	let modalOpen = false;
+	$: modalOpen;
+	export let project: ProjectsResponse;
+</script>
+
+<div class="w-full h-28 flex items-center justify-between">
+	<div class="avatar">
+		<div class="w-20 rounded">
+			<img
+				src={project.thumbnail
+					? getImageUrl(project.collectionId, project.id, project.thumbnail, '80x80')
+					: `https://via.placeholder.com/80/4506cb/FFFFFF/?text=${project.name}`}
+				alt="thumbnail"
+			/>
+		</div>
+	</div>
+	<div class="flex flex-col w-full ml-4 h-full justify-center">
+		<a href="/project/{project.id}" class="font-semibold text-lg">{project.name}</a>
+		<p>{project.tagline}</p>
+	</div>
+	<div class="flex items-center justify-end w-full">
+		<a href="/projects/{project.id}/edit" class="btn btn-outline">Edit Project</a>
+		<Modal label={project.id} checked={modalOpen}>
+			<span slot="trigger" class="btn btn-error ml-2">Delete</span>
+			<div slot="heading">
+				<h3 class="text-2xl">Delete {project.name}</h3>
+				<p class="text-base font-normal mt-2">
+					Are you sure you want to delete this project? Once deleted, the project cannot be
+					restored.
+				</p>
+			</div>
+			<div slot="actions" class="flex w-full items-center justify-center space-x-2">
+				<label for={project.id} class="btn btn-outline">Cancel</label>
+				<form action="?/deleteProject" method="POST" class="btn btn-error">
+					<input type="hidden" name="id" value={project.id} />
+					<button class="btn btn-error">Delete</button>
+				</form>
+			</div>
+		</Modal>
+	</div>
+</div>
